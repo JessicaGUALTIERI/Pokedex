8d93360cbba1cf197d92bcc2cb588ab8e62b7854
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-08 15:54:48 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent, useState } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+  
+const App: FunctionComponent = () => {
+ const [pokemons] = useState<Pokemon[]>(POKEMONS);
+    
+ return (
+  <div>
+    <h1>Pokédex</h1>
+    <p>Il y a { pokemons.length } pokémons dans le Pokédex.</p>
+  </div>
+ )
+}
+
+  
+export default App;
