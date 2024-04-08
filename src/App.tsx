346d4a16f19927b111b06f7fd9c55ffd83720c4f
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-08 19:04:48 +0200

    Utilisation de useEffect (hook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
   
 const App: FunctionComponent = () => {
- const [pokemons] = useState<Pokemon[]>(POKEMONS);
-    
+ const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+ useEffect(() => {
+    setPokemons(POKEMONS);
+ }, []);
  return (
   <div>
     <h1>Pokédex</h1>
@@ -13,5 +15,4 @@ const App: FunctionComponent = () => {
  )
 }
 
-  
 export default App;
