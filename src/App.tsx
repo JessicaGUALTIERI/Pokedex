31a24b9f6cda4c9838177990dddf56c50842948e
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-09 14:06:16 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
   
 const App: FunctionComponent = () => {
  return (
-  <PokemonList></PokemonList>
+  <Router>
+    <div>
+        {}
+        <nav>
+            <div className='nav-wrapper teal'>
+                <Link to="/" className="brand-logo center">Pokédex</Link>
+            </div>
+        </nav>
+        {}
+        <Switch>
+            <Route exact path="/" component={ PokemonList }></Route>
+            <Route exact path="/pokemons" component={ PokemonList }></Route>
+            <Route path="/pokemons/:id" component={ PokemonDetail }></Route>
+        </Switch>
+    </div>
+  </Router>
  )
 }
 
